286679ddc3df560600aff3810e1f757f86362940
Arindam Ghosh <ghosharindam195@gmail.com>
2023-01-26 12:25:56 +0530

    ready to deploy

--- chandahotel_and_lodging/wsgi.py
+++ chandahotel_and_lodging/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chandahotel_and_lodging.settings')
 
 application = get_wsgi_application()
+app = application
